5b38b88652590197e07b533815fc24a4b156ef30
Shashank Srivastav <shashankgrovy@gmail.com>
2014-01-03 20:02:52 +0530

    fixes to navigation menu bugs

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+/*---------------------------------------------------------------
+    A simple script for making clickable navigation menu elements
+---------------------------------------------------------------*/
+//DOM loaded
+$(document).ready(function() {
+
+    /* attach click event to buttons */
+    /* menu1 */
+    $('li.clicker1').click(function(e) {
+            /* Close menu2 if it is visible first and then open menu1 */
+            if ($('#menu2 li').is(':visible')) {
+                $('li.clicker2').toggleClass('active');
+                $("#menu2 li").fadeToggle(1);
+            }
+            /* show menu1 */
+            $('li.clicker1').toggleClass('active');
+            $("#menu1 li").fadeToggle(300);
+            e.stopPropagation();
+    });
+
+    /* menu2 */
+    $('li.clicker2').click(function(e) {
+            /* Close menu1 if it is visible first and then open menu2 */
+            if ($('#menu1 li').is(':visible')) {
+                $('li.clicker1').toggleClass('active');
+                $("#menu1 li").fadeToggle(1);
+            }
+            /* show menu2 */
+            $('.clicker2').toggleClass('active');
+            $("#menu2 li").fadeToggle(300);
+            e.stopPropagation();
+    });
+
+    /* hide the submenus if clicked anywhere else on the document */
+    $(document).click(function () {
+            if ($('#menu1 li').is(':visible')) {
+                $('li.clicker1').toggleClass('active');
+                $("#menu1 li").fadeToggle(1);
+            }
+            if ($('#menu2 li').is(':visible')) {
+                $('li.clicker2').toggleClass('active');
+                $("#menu2 li").fadeToggle(1);
+            }
+    });
+});
+
