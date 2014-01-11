d0b436a9c2b0e65176592773409501f269aaff47
Sourabh Verma <sourabh.coder@gmail.com>
2014-01-11 14:53:42 +0530

    Minor Improvements - Makes HTML valid - Adds 'Voltaire' & '47' fonts for offline use - Changes style of #site-logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,52 @@
 /*---------------------------------------------------------------
-    A simple script for making clickable navigation menu elements
+	A simple script for making clickable navigation menu elements
 ---------------------------------------------------------------*/
 //DOM loaded
 $(document).ready(function() {
 
-    /* attach click event to buttons */
-    /* menu1 */
-    $('li.clicker1').click(function(e) {
-            /* Close menu2 if it is visible first and then open menu1 */
-            if ($('#menu2 li').is(':visible')) {
-                $('li.clicker2').toggleClass('active');
-                $("#menu2 li").fadeToggle(1);
-            }
-            /* show menu1 */
-            $('li.clicker1').toggleClass('active');
-            $("#menu1 li").fadeToggle(300);
-            e.stopPropagation();
-    });
+	/* attach click event to buttons */
+	/* menu1 */
+	var $menu1 = $('#menu1');
+	var $menu1button = $('#menu1button');
+	var $menu2 = $('#menu2');
+	var $menu2button = $('#menu2button');
 
-    /* menu2 */
-    $('li.clicker2').click(function(e) {
-            /* Close menu1 if it is visible first and then open menu2 */
-            if ($('#menu1 li').is(':visible')) {
-                $('li.clicker1').toggleClass('active');
-                $("#menu1 li").fadeToggle(1);
-            }
-            /* show menu2 */
-            $('.clicker2').toggleClass('active');
-            $("#menu2 li").fadeToggle(300);
-            e.stopPropagation();
-    });
+	$menu1button.click(function (e) {
+		e.stopPropagation();
+		/* Close menu2 if it is visible first and then open menu1 */
+		if ($menu2.is(':visible')) {
+			$menu2button.toggleClass('active');
+			$menu2.hide();
+		}
+		/* show menu1 */
+		$menu1button.toggleClass('active');
+		$menu1.fadeToggle(300);
+	});
 
-    /* hide the submenus if clicked anywhere else on the document */
-    $(document).click(function () {
-            if ($('#menu1 li').is(':visible')) {
-                $('li.clicker1').toggleClass('active');
-                $("#menu1 li").fadeToggle(1);
-            }
-            if ($('#menu2 li').is(':visible')) {
-                $('li.clicker2').toggleClass('active');
-                $("#menu2 li").fadeToggle(1);
-            }
-    });
+	/* menu2 */
+	$menu2button.click(function (e) {
+		e.stopPropagation();
+		/* Close menu1 if it is visible first and then open menu2 */
+		if ($menu1.is(':visible')) {
+			$menu1button.toggleClass('active');
+			$menu1.hide();
+		}
+		/* show menu2 */
+		$menu2button.toggleClass('active');
+		$menu2.fadeToggle(300);
+	});
+
+	/* hide the submenus if clicked anywhere else on the document */
+	$(document).click(function (e) {
+		if (! $(e.target).is('footer')) {
+			if ($menu1.is(':visible')) {
+				$menu1button.toggleClass('active');
+				$menu1.fadeToggle(1);
+			}
+			if ($menu2.is(':visible')) {
+				$menu2button.toggleClass('active');
+				$menu2.fadeToggle(1);
+			}
+		}
+	});
 });
